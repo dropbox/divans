bfb8074bea2b93a38ddccf86d5e840c51e9573e3
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2017-06-04 01:12:22 -0700

    make dictionary lookup codepath

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@ extern crate alloc_no_stdlib as alloc;
 extern crate brotli_decompressor;
 pub use brotli_decompressor::{BrotliResult};
 pub use alloc::{AllocatedStackMemory, Allocator, SliceWrapper, SliceWrapperMut, StackAllocator};
+use brotli_decompressor::dictionary::{kBrotliMaxDictionaryWordLength, kBrotliDictionary,
+                                      kBrotliDictionaryOffsetsByLength};
+use brotli_decompressor::transform::{TransformDictionaryWord};
 
 #[derive(Debug)]
 pub struct CopyCommand {
@@ -82,9 +85,31 @@ impl<RingBuffer: SliceWrapperMut<u8> + SliceWrapper<u8> + Default> DivansRecodeS
         }
         BrotliResult::ResultSuccess
     }
-    fn copy_to_ring_buffer(&mut self, data: &[u8]) -> usize {
-        //FIXME
-        panic!("unimplemented");
+    fn decode_space_left_in_ring_buffer(&self) -> usize {
+        if self.ring_buffer_output_index < self.ring_buffer_decode_index {
+            return self.ring_buffer_output_index as usize + self.ring_buffer.slice().len() - 1 - self.ring_buffer_decode_index as usize;
+        }
+        return self.ring_buffer_output_index as usize - 1 - self.ring_buffer_decode_index as usize;
+    }
+
+    // takes in a buffer of data to copy to the ring buffer--returns the number of bytes persisted
+    fn copy_to_ring_buffer(&mut self, mut data: &[u8]) -> usize {
+        data = data.split_at(core::cmp::min(data.len(), self.decode_space_left_in_ring_buffer())).0;
+        let mut retval = 0usize;
+        let first_section = self.ring_buffer.slice_mut().len() - self.ring_buffer_decode_index as usize;
+        let amount_to_copy = core::cmp::min(data.len(), first_section);
+        let (data_first, data_second) = data.split_at(amount_to_copy);
+        self.ring_buffer.slice_mut()[self.ring_buffer_decode_index as usize .. (self.ring_buffer_decode_index as usize + amount_to_copy)].clone_from_slice(data_first);
+        self.ring_buffer_decode_index += amount_to_copy as u32;
+        retval += amount_to_copy;
+        if self.ring_buffer_decode_index == self.ring_buffer.slice().len() as u32 {
+            self.ring_buffer_decode_index = 0;
+            let second_amount_to_copy = data_second.len();
+            self.ring_buffer.slice_mut()[self.ring_buffer_decode_index as usize .. (self.ring_buffer_decode_index as usize + second_amount_to_copy)].clone_from_slice(data_second.split_at(second_amount_to_copy).0);
+            self.ring_buffer_decode_index += second_amount_to_copy as u32;
+            retval += second_amount_to_copy;
+        }
+        retval
     }
     fn parse_literal<SliceType:alloc::SliceWrapper<u8>>(&mut self,
                                                         lit:&LiteralCommand<SliceType>) -> BrotliResult {
@@ -105,8 +130,31 @@ impl<RingBuffer: SliceWrapperMut<u8> + SliceWrapper<u8> + Default> DivansRecodeS
         panic!("unimplemented");
         BrotliResult::ResultSuccess
     }
-    fn parse_dictionary(&mut self, copy:&DictCommand) -> BrotliResult {
-        panic!("unimplemented");
+    fn parse_dictionary(&mut self, dict_cmd:&DictCommand) -> BrotliResult {
+        // dictionary words are bounded in size: make sure there's enough room for the whole word
+        if self.input_sub_offset != 0 {
+            // error: dictionary should never allow for partial words, since they fit in a small amount of space
+            return BrotliResult::ResultFailure;
+        }
+        if self.decode_space_left_in_ring_buffer() < kBrotliMaxDictionaryWordLength as usize + 13 {
+            return BrotliResult::NeedsMoreOutput;
+        }
+        let copy_len = dict_cmd.word_size as u32;
+        let word_len_category_index = kBrotliDictionaryOffsetsByLength[copy_len as usize] as u32;
+        let word_index = (dict_cmd.word_id * copy_len) + word_len_category_index;
+        let dict = &kBrotliDictionary;
+        let word = &dict[(word_index as usize)..(word_index as usize + copy_len as usize)];
+        let mut transformed_word = [0u8;kBrotliMaxDictionaryWordLength as usize + 13];
+        let final_len = TransformDictionaryWord(&mut transformed_word[..],
+                                                &word[..],
+                                                copy_len as i32,
+                                                dict_cmd.transform as i32);
+        if dict_cmd.final_size != 0 && final_len as usize != dict_cmd.final_size as usize {
+            return BrotliResult::ResultFailure;
+        }
+        if self.copy_to_ring_buffer(transformed_word.split_at(final_len as usize).0) as i32 != final_len {
+            panic!("We already assured sufficient space in buffer for word: internal error");
+        }
         BrotliResult::ResultSuccess
     }
     fn parse_command<SliceType:alloc::SliceWrapper<u8>>(&mut self, cmd: &Command<SliceType>) -> BrotliResult {
