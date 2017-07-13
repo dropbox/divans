25dd1088eb34c4e50ce0eeb4831d7e7ea93a71c1
Jongmin Baek <jongmin@dropbox.com>
2017-07-13 14:02:09 -0700

    Tweak initial CDF for FrequentistCDF16

--- src/probability.rs
+++ src/probability.rs
@@ -192,7 +192,7 @@ pub struct FrequentistCDF16 {
 impl Default for FrequentistCDF16 {
     fn default() -> Self {
         FrequentistCDF16 {
-            cdf: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+            cdf: [4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64]
         }
     }
 }
