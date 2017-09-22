dd54cd9c65bfdcc42645d1edd5444704e4d006ed
Anatoly Yakovenko <anatoly@dropbox.com>
2017-09-22 22:43:07 +0000

    update

--- src/codec.rs
+++ src/codec.rs
@@ -875,7 +875,7 @@ impl<AllocU8:Allocator<u8>,
                                     prob = prob * p;
                                 }
                                 ecdf.init(cur_nibble, prob, nibble_prob);
-                                superstate.coder.get_or_put_nibble(&mut cur_nibble, ecdf, billing);
+                                superstate.coder.get_or_put_nibble(&mut cur_nibble, &ecdf, billing);
                             } else {
                                 superstate.coder.get_or_put_nibble(&mut cur_nibble, nibble_prob, billing);
                             }
