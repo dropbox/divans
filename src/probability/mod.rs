bd35180a4657f53362f27b4f6236cc565ca882d4
Daniel Reiter Horn <danielrh@dropbox.com>
2017-11-24 17:42:28 -0800

    add test for averaging in cdfland

--- src/probability/mod.rs
+++ src/probability/mod.rs
@@ -32,12 +32,13 @@ pub use self::external_cdf::ExternalProbCDF16;
 pub use self::simd_frequentist_cdf::SIMDFrequentistCDF16;
 pub use self::opt_frequentist_cdf::OptFrequentistCDF16;
 mod test {
-    use super::{BaseCDF, CDF16, Speed};
+    use super::{BaseCDF, CDF16, BLEND_FIXED_POINT_PRECISION};
     use super::blend_cdf::{BlendCDF16, to_blend, to_blend_lut};
     use super::frequentist_cdf::FrequentistCDF16;
     use super::opt_frequentist_cdf::OptFrequentistCDF16;
         #[cfg(feature="simd")]
     use super::simd_frequentist_cdf::SIMDFrequentistCDF16;
+    use super::Speed; //super speed!
     #[test]
     fn test_blend_lut() {
         for i in 0..16 {
@@ -48,7 +49,56 @@ mod test {
             }
         }
     }
-
+    fn assert_cdf_eq<CDFA: CDF16, CDFB: CDF16>(cdf0: &CDFA, cdf1: &CDFB) {
+       assert_eq!(cdf0.max(), cdf1.max());
+       for sym in 0..16 {
+          assert_eq!(cdf0.cdf(sym as u8), cdf1.cdf(sym as u8));
+       }
+       assert!(cdf0.valid());
+       assert!(cdf1.valid());
+    }
+    fn operation_test_helper<CDFA: CDF16, CDFB: CDF16> (cdf0a: &mut CDFA, cdf1a: &mut CDFA, cdf0b: &mut CDFB, cdf1b: &mut CDFB) {
+        assert_cdf_eq(cdf0a, cdf0b);
+        assert_cdf_eq(cdf1a, cdf1b);
+        let symbol_buffer0 = [0u8, 0u8, 0u8, 0u8, 0u8, 1u8, 2u8, 3u8, 4u8, 5u8, 5u8, 5u8, 5u8, 5u8, 5u8, 6u8, 7u8, 8u8, 8u8,
+                             9u8, 9u8, 10u8, 10u8, 10u8, 10u8, 10u8, 10u8, 10u8, 10u8, 10u8, 10u8, 11u8, 12u8, 12u8, 12u8,
+                             13u8, 13u8, 13u8, 14u8, 15u8, 15u8, 15u8, 15u8, 15u8, 15u8, 15u8];
+        let symbol_buffer1 = [0u8, 0u8, 0u8, 0u8, 0u8, 1u8, 2u8, 3u8, 4u8, 5u8, 5u8, 5u8, 5u8, 5u8, 5u8];
+        for sym in symbol_buffer0.iter() {
+              cdf0a.blend(*sym, Speed::MED);
+              cdf0b.blend(*sym, Speed::MED);
+              assert_cdf_eq(cdf0a, cdf0b);
+        }
+        assert_cdf_eq(&cdf0a.average(cdf1a, (1<<BLEND_FIXED_POINT_PRECISION)>>2), &cdf0b.average(cdf1b, (1<<BLEND_FIXED_POINT_PRECISION)>>2));
+        for sym in symbol_buffer1.iter() {
+              cdf0a.blend(*sym, Speed::MED);
+              cdf0b.blend(*sym, Speed::MED);
+              assert_cdf_eq(cdf0a, cdf0b);
+        }
+        let all = (1<<BLEND_FIXED_POINT_PRECISION);
+        let half = (1<<BLEND_FIXED_POINT_PRECISION)>>1;
+        let quarter = (1<<BLEND_FIXED_POINT_PRECISION)>>2;
+        let threequarters = half + quarter;;
+        
+        assert_cdf_eq(&cdf0a.average(cdf1a, quarter), &cdf0b.average(cdf1b, quarter));
+        assert_cdf_eq(&cdf0a.average(cdf1a, half), &cdf0b.average(cdf1b, half));
+        assert_cdf_eq(&cdf0a.average(cdf1a, threequarters), &cdf0b.average(cdf1b, threequarters));
+        assert_cdf_eq(&cdf0a.average(cdf1a, 0), &cdf0b.average(cdf1b, 0));
+        assert_cdf_eq(&cdf0a.average(cdf1a, all), &cdf0b.average(cdf1b, all));
+    }
+    #[cfg(feature="simd")]
+    #[test]
+    fn test_cdf_simd_eq_opt() {
+       operation_test_helper(&mut SIMDFrequentistCDF16::default(),
+       &mut SIMDFrequentistCDF16::default(),
+       &mut OptFrequentistCDF16::default(),
+       &mut OptFrequentistCDF16::default());
+    }
+    #[test]
+    fn test_cdf_opt_eq_baseline() {
+       operation_test_helper(&mut FrequentistCDF16::default(),&mut FrequentistCDF16::default(),
+                             &mut OptFrequentistCDF16::default(), &mut OptFrequentistCDF16::default());
+    }
     #[allow(unused)]
     const RAND_MAX : u32 = 32_767;
     #[allow(unused)]
