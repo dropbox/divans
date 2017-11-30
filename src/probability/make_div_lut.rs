d7a30c1076f4c964a54b989d6ed8b6a91a28e600
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2017-11-30 00:41:07 -0800

    made a 15 bit div by 8 bit lut

--- src/probability/make_div_lut.rs
+++ src/probability/make_div_lut.rs
@@ -1,5 +1,19 @@
 mod numeric;
 fn main() {
+    print!("pub static RECIPROCAL8: [(i32, u8); 256] = [\n    (0,0), ");
+    for divisor in 1..256 {
+        let next_str = if divisor % 16 == 15 {
+           "\n    "
+        } else {
+           " "
+        };
+        let reciprocal = numeric::compute_divisor8(divisor as numeric::Denominator8Type);
+        for num in 0u16..32768u16 {
+            assert_eq!(num as i16 /divisor, numeric::fast_divide_15bit_by_8bit(num as i16, reciprocal));
+        }
+        print!("({},{}),{}", reciprocal.0, numeric::compute_divisor8(divisor as numeric::Denominator8Type).1, next_str)
+    }
+    print!("];\n");
     print!("pub static RECIPROCAL: [(i64, u8); 65536] = [\n    (0,0), ");
     for divisor in 1..65536 {
         let next_str = if divisor % 16 == 15 {
--- src/probability/numeric.rs
+++ src/probability/numeric.rs
@@ -36,3 +36,28 @@ pub fn fast_divide_30bit_i64x2_by_16bit(num: simd::i64x2, inv_denom_and_bitlen: 
          + ((num - (idiv_mul_num_shift_max_num)) >> 1))
       >> inv_denom_and_bitlen.1
 }
+
+
+
+
+pub type Denominator8Type = u8;
+const LOG_MAX_NUMERATOR16:usize = 15;
+fn k8bit_length(d:Denominator8Type) -> u8 {
+    (8 - d.leading_zeros()) as u8
+}
+pub fn compute_divisor8(d: Denominator8Type) -> (i32, u8) {
+    let bit_len = k8bit_length(d);
+    (((((( 1i32 << bit_len) - i32::from(d)) << (LOG_MAX_NUMERATOR16))) / i32::from(d)) + 1, bit_len.wrapping_sub(1))
+}
+#[cfg(not(feature="division_table_gen"))]
+pub fn lookup_divisor8(d: u8) -> (i32, u8) {
+    div_lut::RECIPROCAL8[d as u8 as usize]
+}
+#[inline(always)]
+pub fn fast_divide_15bit_by_8bit(num: i16, inv_denom_and_bitlen: (i32, u8)) -> i16 {
+    let idiv_mul_num = i32::from(inv_denom_and_bitlen.0) * i32::from(num);
+     ((idiv_mul_num >> LOG_MAX_NUMERATOR16) as i16
+         + (((i32::from(num) - (idiv_mul_num >> LOG_MAX_NUMERATOR16)) as i16) >> 1))
+      >> inv_denom_and_bitlen.1
+}
+
