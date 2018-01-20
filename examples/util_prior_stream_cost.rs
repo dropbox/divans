b39a267a740b2e187cdb143b1ceda4381743c0ea
Daniel Reiter Horn <danielrh@dropbox.com>
2018-01-19 18:27:04 -0800

    if speed is not passed in, then search for the optimal speed

--- examples/util_prior_stream_cost.rs
+++ examples/util_prior_stream_cost.rs
@@ -14,7 +14,7 @@ fn determine_cost(cdf: &divans::DefaultCDF16,
 
 fn eval_stream<Reader:std::io::BufRead>(
     r :&mut Reader,
-    speed: divans::Speed,
+    speed: Option<divans::Speed>,
     is_hex: bool
 ) -> Result<f64> {
     let mut sub_streams = HashMap::<u64, vec::Vec<u8>>::new();
@@ -68,22 +68,48 @@ fn eval_stream<Reader:std::io::BufRead>(
             }
         }
     }
+    let specified_speed = match speed {
+        Some(s) => [s],
+        None => [divans::Speed::MUD],
+    };
+    let trial_speeds = [divans::Speed::GEOLOGIC, divans::Speed::GLACIAL, divans::Speed::MUD, divans::Speed::SLOW,
+                        divans::Speed::MED, divans::Speed::FAST, divans::Speed::PLANE, divans::Speed::ROCKET];
+    let speed_choice = match speed {
+        Some(_) => &specified_speed[..],
+        None => &trial_speeds[..],
+    };
     for (&prior, sub_stream) in sub_streams.iter() {
-        for val in sub_stream.iter() {
-            let val_nibbles = (val >> 4, val & 0xf);
-            let prior_index_0 = (prior, 0xff);
-            let prior_index_1 = (prior, val_nibbles.0);
-            {
-                let mut cdf0 = &mut stream_state.entry(prior_index_0).or_insert(divans::DefaultCDF16::default());
-                cost += determine_cost(cdf0, val_nibbles.0);
-                cdf0.blend(val_nibbles.0, speed);
+        let mut best_cost_high: Option<f64> = None;
+        let mut best_cost_low: Option<f64> = None;
+        for cur_speed in speed_choice.iter() {
+            let mut cur_cost_high: f64 = 0.0;
+            let mut cur_cost_low: f64 = 0.0;
+            for val in sub_stream.iter() {
+                let val_nibbles = (val >> 4, val & 0xf);
+                let prior_index_0 = (prior, 0xff);
+                let prior_index_1 = (prior, val_nibbles.0);
+                {
+                    let mut cdf0 = &mut stream_state.entry(prior_index_0).or_insert(divans::DefaultCDF16::default());
+                    cur_cost_high += determine_cost(cdf0, val_nibbles.0);
+                    cdf0.blend(val_nibbles.0, *cur_speed);
+                }
+                {
+                    let mut cdf1 = &mut stream_state.entry(prior_index_1).or_insert(divans::DefaultCDF16::default());
+                    cur_cost_low += determine_cost(cdf1, val_nibbles.1);
+                    cdf1.blend(val_nibbles.1, *cur_speed);
+                }
             }
-            {
-                let mut cdf1 = &mut stream_state.entry(prior_index_1).or_insert(divans::DefaultCDF16::default());
-                cost += determine_cost(cdf1, val_nibbles.1);
-                cdf1.blend(val_nibbles.1, speed);
-            }
+            best_cost_high = match best_cost_high.clone() {
+                None => Some(cur_cost_high),
+                Some(bc) => Some(if bc > cur_cost_high {cur_cost_high} else {bc}),
+            };
+            best_cost_low = match best_cost_low.clone() {
+                None => Some(cur_cost_low),
+                Some(bc) => Some(if bc > cur_cost_low {cur_cost_low} else {bc}),
+            };
         }
+        cost += best_cost_high.unwrap();
+        cost += best_cost_low.unwrap();
     }
     Ok(cost)
 }
@@ -93,10 +119,10 @@ fn main() {
     let stdin = std::io::stdin();
     let stdin = stdin.lock();
     let mut buffered_in = BufReader::new(stdin);
-    let mut speed = divans::Speed::MUD;
+    let mut speed: Option<divans::Speed> = None;
     if env::args_os().len() > 1 {
         for argument in env::args().skip(1) {
-            speed  = argument.parse::<divans::Speed>().unwrap();
+            speed = Some(argument.parse::<divans::Speed>().unwrap());
         }
     }
     let cost = eval_stream(&mut buffered_in, speed, true).unwrap();
