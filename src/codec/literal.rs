62d297ca08e4692603d524231c59ebfde27e2f2c
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2018-04-01 13:50:03 -0700

    allow slowed down speed

--- src/codec/literal.rs
+++ src/codec/literal.rs
@@ -145,8 +145,8 @@ impl<AllocU8:Allocator<u8>,
         }
         let mm_opts = (superstate.bk.mixing_mask[(mixing_mask_index >> 5)] >> ((mixing_mask_index & 31) * 2)) & 3;
         let is_mm = (mm_opts & 1) as usize; 
-        let spd = if false && (mm_opts & 2) != 0 {
-            Speed::new(0,32)
+        let spd = if mm_opts == 2 {
+            Speed::new(1,48)
         } else {
             superstate.bk.literal_adaptation[(((!is_mm)&1) << 1) | high_nibble as usize].clone()
         };
