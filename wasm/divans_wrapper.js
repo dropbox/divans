23a81b74d0725d80b47c49da897d3b0199bed1a0
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2018-05-21 23:00:21 -0700

    fix bug with large files not using all their input

--- wasm/divans_wrapper.js
+++ wasm/divans_wrapper.js
@@ -1,6 +1,11 @@
 var wasm_divans_bytecode = '../target/wasm32-unknown-unknown/release/divans.wasm';
 var options_map;
 var desired_option_list = [];
+var BUFFER_SIZE = 16384;
+var max_quality = 11;
+function set_max_quality(q) {
+   max_quality = q;
+}
 function is_divans(file) {
     return file.length > 16 && file[0] == 255 && file[1] == 229 && file[2] == 140 && file[3] == 159;
 }
@@ -21,18 +26,19 @@ function encode(external_uint8_buffer, obj_instance_exports, debug, encoder_opti
        }
     }
     var input_ptr = divans.divans_compressor_malloc_u8(state, external_uint8_buffer.length);
-    var output_ptr = divans.divans_compressor_malloc_u8(state, 4096);
+    var output_ptr = divans.divans_compressor_malloc_u8(state, BUFFER_SIZE);
     var input_offset_ptr = divans.divans_compressor_malloc_usize(state, 1);
     var output_offset_ptr = divans.divans_compressor_malloc_usize(state, 1);
     var input_buf = new Uint8Array(divans.memory.buffer, input_ptr, external_uint8_buffer.length);
-    var output_buf = new Uint8Array(divans.memory.buffer, output_ptr, 4096);
+    var output_buf = new Uint8Array(divans.memory.buffer, output_ptr, BUFFER_SIZE);
     var input_offset_buf = new Uint32Array(divans.memory.buffer, input_offset_ptr, 1);
     input_buf.set(external_uint8_buffer)
     var output_size = 0;
     var ret = 0;
     do {
        ret = divans.divans_encode(state, input_ptr, external_uint8_buffer.length, input_offset_ptr,
-                      output_ptr, 4096, output_offset_ptr);
+                      output_ptr, BUFFER_SIZE, output_offset_ptr);
+
        var output_offset_buf = new Uint32Array(divans.memory.buffer,output_offset_ptr, 1);
        output_size += output_offset_buf[0];
        if (debug) {
@@ -50,10 +56,17 @@ function encode(external_uint8_buffer, obj_instance_exports, debug, encoder_opti
             list_of_encoded_buffers[list_of_encoded_buffers.length] = new Uint8Array(produced_data);
        }
        output_offset_buf[0] = 0;
-    } while(ret != 0 && ret != 1);
+       var input_offset_buf = new Uint32Array(divans.memory.buffer,input_offset_ptr, 1);
+       if (input_offset_buf[0] == external_uint8_buffer.length) {
+         break;
+       }
+       if (debug) {
+           console.log("working:" + input_offset_buf[0] + "/" + external_uint8_buffer.length);
+       }
+    } while(ret != 0 && ret != 3);
     do {
        ret = divans.divans_encode_flush(state,
-                      output_ptr, 4096, output_offset_ptr);
+                      output_ptr, BUFFER_SIZE, output_offset_ptr);
        var output_offset_buf = new Uint32Array(divans.memory.buffer,output_offset_ptr, 1);
        output_size += output_offset_buf[0];
        if (output_offset_buf[0] != 0) {
@@ -69,7 +82,7 @@ function encode(external_uint8_buffer, obj_instance_exports, debug, encoder_opti
         console.log("TOTAL SIZE " + output_size);
     }
     divans.divans_compressor_free_u8(state, input_ptr, external_uint8_buffer.length);
-    divans.divans_compressor_free_u8(state, output_ptr, 4096);
+    divans.divans_compressor_free_u8(state, output_ptr, BUFFER_SIZE);
     divans.divans_compressor_free_usize(state, input_offset_ptr, 1);
     divans.divans_compressor_free_usize(state, output_offset_ptr, 1);
     divans.divans_free_compressor(state);
@@ -80,25 +93,25 @@ function decode(external_uint8_buffer, obj_instance_exports, debug) {
     var divans = obj_instance_exports;
     var state = divans.divans_new_serial_decompressor();
     var input_ptr = divans.divans_decompressor_malloc_u8(state, external_uint8_buffer.length);
-    var output_ptr = divans.divans_decompressor_malloc_u8(state, 4096);
+    var output_ptr = divans.divans_decompressor_malloc_u8(state, BUFFER_SIZE);
     var input_offset_ptr = divans.divans_decompressor_malloc_usize(state, 1);
     var output_offset_ptr = divans.divans_decompressor_malloc_usize(state, 1);
     var input_buf = new Uint8Array(divans.memory.buffer, input_ptr, external_uint8_buffer.length);
-    var output_buf = new Uint8Array(divans.memory.buffer, output_ptr, 4096);
+    var output_buf = new Uint8Array(divans.memory.buffer, output_ptr, BUFFER_SIZE);
     var input_offset_buf = new Uint32Array(divans.memory.buffer, input_offset_ptr, 1);
     input_buf.set(external_uint8_buffer)
     var output_size = 0;
     var ret = 0;
     do {
        ret = divans.divans_decode(state, input_ptr, external_uint8_buffer.length, input_offset_ptr,
-                      output_ptr, 4096, output_offset_ptr);
-       var output_offset_buf = new Uint32Array(divans.memory.buffer,output_offset_ptr, 1);
-       output_size += output_offset_buf[0];
+                      output_ptr, BUFFER_SIZE, output_offset_ptr);
+       var input_offset_buf = new Uint32Array(divans.memory.buffer,input_offset_ptr, 1);
        if (debug) {
             console.log(ret);
        }
        var output_offset_buf = new Uint32Array(divans.memory.buffer,output_offset_ptr, 1);
        if (output_offset_buf[0] !== 0) {
+            output_size += output_offset_buf[0];
             if (debug) {
                 console.log(output_offset_buf[0]);
             }
@@ -109,12 +122,15 @@ function decode(external_uint8_buffer, obj_instance_exports, debug) {
             list_of_decoded_buffers[list_of_decoded_buffers.length] = new Uint8Array(produced_data);
        }
        output_offset_buf[0] = 0;
-    } while(ret != 0 && ret != 1 && ret != 3);
+       if (ret == 1  & input_offset_buf[0] !=  external_uint8_buffer.length) {
+          break; // we need more input, but we have no more input to give
+       }
+    } while(ret != 0 && ret != 3);
     if (debug) {
         console.log("TOTAL SIZE " + output_size);
     }
     divans.divans_decompressor_free_u8(state, input_ptr, external_uint8_buffer.length);
-    divans.divans_decompressor_free_u8(state, output_ptr, 4096);
+    divans.divans_decompressor_free_u8(state, output_ptr, BUFFER_SIZE);
     divans.divans_decompressor_free_usize(state, input_offset_ptr, 1);
     divans.divans_decompressor_free_usize(state, output_offset_ptr, 1);
     divans.divans_free_decompressor(state);
